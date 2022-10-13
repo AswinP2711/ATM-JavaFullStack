2aca7b25de7b83c9290e414c099d8a64381ba78d
Aswin P <2000078293@hexaware.com>
2022-10-13 17:01:22 +0530

    Hello World

---
+++ AppointmentScheduleSystem/src/test/java/com/ass/api/AppointmentScheduleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ass.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentScheduleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
